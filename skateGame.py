c12232c3bd84fe31d5abed60c30ff37da1a598e3
Arthur Fagundes <dev.arthurfagundes@gmail.com>
2022-06-22 01:57:07 -0300

    Print do skate em tela e feature das nuvens se moverem pelo cenario

--- skateGame.py
+++ skateGame.py
@@ -1,6 +1,12 @@
 import pygame
 from pygame.locals import *
 from sys import exit
+import os
+from random import randrange
+
+main_directory = os.path.dirname(__file__)
+images_directory = os.path.join(main_directory, "images")
+sounds_dorectory = os.path.join(main_directory, "sounds")
 
 width = 640
 height = 480
@@ -11,6 +17,47 @@ window = pygame.display.set_mode((width, height))
 
 pygame.display.set_caption('T-Skate Game')
 
+sprite_sheet = pygame.image.load(os.path.join(
+    images_directory, "skateSpriteSheet.png")).convert_alpha()
+
+
+class Skate(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.skateboard_image = []
+        self.img = sprite_sheet.subsurface((0, 0), (32, 32))
+        self.img = pygame.transform.scale(self.img, (32*3, 32*3))
+        self.skateboard_image.append(self.img)
+
+        self.index_list = 0
+        self.image = self.skateboard_image[self.index_list]
+        self.rect = self.image.get_rect()
+        self.rect.center = (100, height - 90)
+
+class Clounds(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = sprite_sheet.subsurface((3*32, 0), (32, 32))
+        self.image = pygame.transform.scale(self.image, (32*3, 32*3))
+        self.rect = self.image.get_rect()
+        self.rect.y = randrange(50, 200, 50)
+        self.rect.x = width - randrange(32, 320, 92)
+
+    def update(self):
+        if self.rect.topright[0] < 0:
+            self.rect.x = width
+            self.rect.y = randrange(50, 200, 50)
+        self.rect.x -= 10
+
+
+all_sprites = pygame.sprite.Group()
+skateboard = Skate()
+all_sprites.add(skateboard)
+
+for i in range(4):
+    clound = Clounds()
+    all_sprites.add(clound)
+
 clock = pygame.time.Clock()
 while True:
     clock.tick(30)
@@ -19,5 +66,8 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
+
+    all_sprites.draw(window)
+    all_sprites.update()
 
     pygame.display.flip()
