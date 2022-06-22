458252c8f32f53343d0e70ecfb42757a0538fc30
Arthur Fagundes <dev.arthurfagundes@gmail.com>
2022-06-22 03:40:33 -0300

    Adicionado obstaculos e sistema de colisão.

--- skateGame.py
+++ skateGame.py
@@ -23,11 +23,18 @@ pygame.display.set_caption('T-Skate Game')
 sprite_sheet = pygame.image.load(os.path.join(
     images_directory, "skateSpriteSheet.png")).convert_alpha()
 
+collision_sound = pygame.mixer.Sound(
+    os.path.join(sounds_directory, "death_sound.wav"))
+collision_sound.set_volume(1)
+
+collided = False
+
 
 class Skate(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.ollie_sound = pygame.mixer.Sound(os.path.join(sounds_directory, "ollie_sound.wav"))
+        self.ollie_sound = pygame.mixer.Sound(
+            os.path.join(sounds_directory, "ollie_sound.wav"))
         self.ollie_sound.set_volume(1)
         self.skateboard_image = []
         self.img = sprite_sheet.subsurface((0, 0), (32, 32))
@@ -37,17 +44,18 @@ class Skate(pygame.sprite.Sprite):
         self.index_list = 0
         self.image = self.skateboard_image[self.index_list]
         self.rect = self.image.get_rect()
-        self.pos_y_init = height - 32 - 48
+        self.mask = pygame.mask.from_surface(self.image)
+        self.pos_y_init = height - 31 - 48
         self.rect.center = (100, height - 32)
         self.ollie = False
-    
+
     def doOllie(self):
         self.ollie = True
         self.ollie_sound.play()
 
     def update(self):
         if self.ollie == True:
-            if self.rect.y <= 300:
+            if self.rect.y <= 250:
                 self.ollie = False
             self.rect.y -= 20
         else:
@@ -55,6 +63,7 @@ class Skate(pygame.sprite.Sprite):
                 self.rect.y += 20
             else:
                 self.rect.y = self.pos_y_init
+
 
 class Clounds(pygame.sprite.Sprite):
     def __init__(self):
@@ -87,6 +96,21 @@ class Streets(pygame.sprite.Sprite):
         self.rect.x -= 10
 
 
+class Barrier(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = sprite_sheet.subsurface((32, 0), (32, 32))
+        self.image = pygame.transform.scale(self.image, (32*4, 32*4))
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.center = (width, height - 40)
+
+    def update(self):
+        if self.rect.topright[0] < 0:
+            self.rect.x = width
+        self.rect.x -= 10
+
+
 all_sprites = pygame.sprite.Group()
 skateboard = Skate()
 all_sprites.add(skateboard)
@@ -98,6 +122,12 @@ for i in range(4):
 for i in range(12):
     street = Streets(i)
     all_sprites.add(street)
+
+barrier = Barrier()
+all_sprites.add(barrier)
+
+obstacle_group = pygame.sprite.Group()
+obstacle_group.add(barrier)
 
 clock = pygame.time.Clock()
 while True:
@@ -114,7 +144,20 @@ while True:
                 else:
                     skateboard.doOllie()
 
+    collisions = pygame.sprite.spritecollide(
+        skateboard, obstacle_group, False, pygame.sprite.collide_mask)
+
     all_sprites.draw(window)
-    all_sprites.update()
+
+    if collisions and collided == False:
+        collision_sound.play()
+        collided = True
+        pass
+
+    if collided == True:
+        pass
+
+    else:
+        all_sprites.update()
 
     pygame.display.flip()
